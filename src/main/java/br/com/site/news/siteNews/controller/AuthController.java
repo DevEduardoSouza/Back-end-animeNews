fbdfddc00b4c44189e0f3e43d04e8eb0c1ff18c1
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-15 18:38:13 -0300

    Adicionando o métodode login no controller

--- src/main/java/br/com/site/news/siteNews/controller/AuthController.java
+++ src/main/java/br/com/site/news/siteNews/controller/AuthController.java
@@ -4,8 +4,11 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +25,7 @@ import jakarta.validation.Valid;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api")
-public class UserController {
+public class AuthController {
 
     @Autowired
     private IUserFacade userFacade;
@@ -32,16 +35,21 @@ public class UserController {
         return userFacade.getUsers();
     }
 
-    //Salvar usuário
+    //Cadastar usuário
     @PostMapping("/cadastrar")
-    public ResponseEntity<Object> salvarUser(@RequestBody @Valid UserDto userDto){
-        
+    public ResponseEntity<Object> cadastrar(@RequestBody @Valid UserDto userDto){
         UserModel userModel = new UserModel();
         BeanUtils.copyProperties(userDto, userModel);
         userModel.setRegisterDate(LocalDateTime.now(ZoneId.of("UTC")));
 
-
         return userFacade.registerUser(userModel);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(){
+        return ResponseEntity.status(HttpStatus.OK).body("teste");
+    }
+
+    
     
 }
