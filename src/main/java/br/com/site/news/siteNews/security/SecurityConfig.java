fbf42cce5109ca4fe90f03a79a5f42bc84a6c5ec
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-16 17:14:56 -0300

    Implementando o SecurityConfig e doFilterInternal

--- src/main/java/br/com/site/news/siteNews/security/SecurityConfig.java
+++ src/main/java/br/com/site/news/siteNews/security/SecurityConfig.java
@@ -1,11 +1,28 @@
 package br.com.site.news.siteNews.security;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable()               //desabilito o csrf pq que que vou tratar o autenticação do usuários
+                .authorizeHttpRequests()    //agora as ruequisições http são passiveis de autorização
+                .requestMatchers(HttpMethod.POST, "/cadastrar").permitAll() //Especificando quem é liberado
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated().and().cors();
+
+        http.addFilterBefore(new Filter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
