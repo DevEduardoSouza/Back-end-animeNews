b32737b72dd8ed0e8463f1e300a1146204a40663
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-17 13:01:26 -0300

    Atualizando o SecurityConfig para usar lambda

--- src/main/java/br/com/site/news/siteNews/security/SecurityConfig.java
+++ src/main/java/br/com/site/news/siteNews/security/SecurityConfig.java
@@ -15,14 +15,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.csrf().disable()               //desabilito o csrf pq que que vou tratar o autenticação do usuários
-                .authorizeHttpRequests()    //agora as ruequisições http são passiveis de autorização
-                .requestMatchers(HttpMethod.POST, "/cadastrar").permitAll() //Especificando quem é liberado
-                .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                .anyRequest().authenticated().and().cors();
-
-        http.addFilterBefore(new Filter(), UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
+        return http
+                .authorizeHttpRequests(
+                        authorizeConfig -> {
+                            authorizeConfig.requestMatchers("/login").permitAll();
+                            authorizeConfig.requestMatchers("/logout").permitAll();
+                            authorizeConfig.requestMatchers("/cadastrar").permitAll();
+                            authorizeConfig.anyRequest().authenticated();
+                        }
+                )
+                .build();
     }
 }
