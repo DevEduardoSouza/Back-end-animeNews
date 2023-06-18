472552181ab7a7b10ace22f17b22c50c178162a1
Eduardo <103686965+eduardo074@users.noreply.github.com>
2023-06-17 21:27:13 -0300

    Adicionando variáveis de ambiente para acessar oauth2

--- src/main/java/br/com/site/news/siteNews/security/SecurityConfig.java
+++ src/main/java/br/com/site/news/siteNews/security/SecurityConfig.java
@@ -1,28 +1,24 @@
 package br.com.site.news.siteNews.security;
 
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${CLIENT_ID}")
-    private String clientId;
 
-    @Value("${CLIENT_SECRET}")
-    private String clientSecret;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
         return http
                 .authorizeHttpRequests(
                         authorizeConfig -> {
@@ -35,7 +31,7 @@ public class SecurityConfig {
 //                .formLogin(formLogin -> formLogin.loginPage("/api/login")
 //                        .permitAll()
 //                )
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 }
